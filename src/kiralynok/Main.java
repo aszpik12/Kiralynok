c640efdd0354c02975c5aea1208a10536fed01e5
Asztrik <asztrikpetrovai@gmail.com>
2024-02-12 14:22:20 +0100

    sor/oszlop leagaláb egy K keresése

--- src/kiralynok/Main.java
+++ src/kiralynok/Main.java
@@ -5,10 +5,12 @@ public class Main {
     
     public static void main(String[] args) {
         Tabla tabla = new Tabla('#');
-        tabla.Megjelenit();
-        tabla.Elhelyez(6);
+        System.out.printf(tabla.Megjelenit());
+        tabla.Elhelyez(8);
         System.out.println("--------------");
-        tabla.Megjelenit();
+        System.out.printf(tabla.Megjelenit());
+        System.out.println(tabla.UresOszlop(0));
+        System.out.println(tabla.UresSor(0));
     }
     
 }
--- src/kiralynok/Tabla.java
+++ src/kiralynok/Tabla.java
@@ -1,26 +1,27 @@
 package kiralynok;
 
 public class Tabla {
-    private char [][] T;
+
+    private char[][] T;
     private char UresCella;
 
     public Tabla(char UresCella) {
         T = new char[8][8];
         this.UresCella = UresCella;
-        
+
         for (int i = 0; i < T.length; i++) {
             for (int j = 0; j < T.length; j++) {
-                T[i][j]=UresCella;
+                T[i][j] = UresCella;
             }
         }
     }
-    
-    public void Elhelyez(int N){
-        
+
+    public void Elhelyez(int N) {
+
         for (int i = 0; i < N;) {
-        int random = Random(7, 0);
-        int random2 = Random(7, 0);
-            if(T[random][random2] == UresCella){
+            int random = Random(7, 0);
+            int random2 = Random(7, 0);
+            if (T[random][random2] == UresCella) {
                 T[random][random2] = 'K';
                 i++;
             }
@@ -31,31 +32,53 @@ public class Tabla {
         int random = (int) (Math.random() * (max - min) + min);
         return random;
     }
-    
-    public void Fajlbair(){
-        
+
+    public void Fajlbair() {
+
     }
-        
-    public void Megjelenit(){
+
+    public String Megjelenit() {
+        String megjel = "";
         for (int i = 0; i < T.length; i++) {
             for (int j = 0; j < T.length; j++) {
-                System.out.print(T[i][j]);
+                megjel += T[i][j];
             }
-            System.out.println("");
+            megjel += "\n";
         }
+
+        return megjel;
     }
-        
-    public void Tabla(){
-        
+
+    public void Tabla() {
+
     }
-        
-    public void UresOszlop(){
-        
+
+    public String UresOszlop(int oszlop) {
+        String kiir = "Nincs ebben az oszlopban egyetlen egy Királynő se!";
+        if (oszlop > 7 || oszlop < 0) {
+            kiir = "Nem megfelelő oszlop szám!\n";
+        } else {
+            for (int i = 0; i < T.length; i++) {
+                if(T[i][oszlop]=='K'){
+                    kiir = "Van ebben az oszlopban legalább egy Királynő!";
+                }
+            }
+        }
+        return kiir;
     }
-    
-    public void UresSor(){
-        
+
+    public String UresSor(int sor) {
+       String kiir = "Nincs ebben az sorban egyetlen egy Királynő se!";
+        if (sor > 7 || sor < 0) {
+            kiir = "Nem megfelelő sorszám!\n";
+        } else {
+            for (int i = 0; i < T.length; i++) {
+                if(T[sor][i]=='K'){
+                    kiir = "Van ebben a sorban legalább egy Királynő!";
+                }
+            }
+        }
+        return kiir;
     }
-    
-    
+
 }
