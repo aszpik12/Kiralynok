401a4084f131aa24eb05b5dd8219defd9001e659
Asztrik <asztrikpetrovai@gmail.com>
2024-02-12 13:28:37 +0100

    konstruktor elkészítése

---
+++ src/kiralynok/Main.java
@@ -0,0 +1,11 @@
+
+package kiralynok;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Tabla feladat = new Tabla('a');
+        System.out.println(feladat);
+    }
+    
+}
--- src/kiralynok/Tabla.java
+++ src/kiralynok/Tabla.java
@@ -1,6 +1,20 @@
 package kiralynok;
 
 public class Tabla {
-    private int [][] T = new int[2][3];
+    private char [][] T;
     private char UresCella;
+
+    public Tabla(char UresCella) {
+        T = new char[8][8];
+        this.UresCella = UresCella;
+        
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T.length; j++) {
+                T[i][j]=UresCella;
+            }
+            
+        }
+    }
+    
+    
 }
