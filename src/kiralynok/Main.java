0ee13c6c3892a7b7602dd725ed553f62d1094ef2
Asztrik <asztrikpetrovai@gmail.com>
2024-02-12 13:39:07 +0100

    megjelenites nem guisan (proba)

--- src/kiralynok/Main.java
+++ src/kiralynok/Main.java
@@ -4,8 +4,8 @@ package kiralynok;
 public class Main {
     
     public static void main(String[] args) {
-        Tabla feladat = new Tabla('a');
-        System.out.println(feladat);
+        Tabla tabla = new Tabla('#');
+        tabla.Megjelenit();
     }
     
 }
--- src/kiralynok/Tabla.java
+++ src/kiralynok/Tabla.java
@@ -12,8 +12,36 @@ public class Tabla {
             for (int j = 0; j < T.length; j++) {
                 T[i][j]=UresCella;
             }
-            
         }
+    }
+    
+    public void Elhelyez(){
+        
+    }
+    
+    public void Fajlbair(){
+        
+    }
+        
+    public void Megjelenit(){
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T.length; j++) {
+                System.out.print(T[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+        
+    public void Tabla(){
+        
+    }
+        
+    public void UresOszlop(){
+        
+    }
+    
+    public void UresSor(){
+        
     }
     
     
